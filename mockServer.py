bc45bb530f614b75513f495b78cf3369231055af
hailin.wang <hailin.wang@fengjr.com>
2019-01-04 09:50:29 +0800

    经典版web

--- mockServer.py
+++ mockServer.py
@@ -5,3 +5,17 @@
 @file: mockServer.py
 @time: 2019/01/04
 """
+import  web
+
+#元组URL规则
+urls = {
+    '/','index',
+}
+
+class index:
+    def GET(self):
+        return 'hello word!'
+
+if __name__ == "__main__":
+    app = web.application(urls,globals())
+    app.run()
